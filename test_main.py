d2eb2dd5aa2eb132dcd57480ba779db5a5ee2d5f
youngbad <jakubbadowski.jb@gmail.com>
2026-03-16 12:06:48 +0100

    chore: Remove unused test files and add .gitkeep to plugins directory

--- test_main.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.write("hello")
-if __name__ == "__main__":
-    st.write("Main works!")
--- test_st.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-st.write("Hello")
-st.write("Name is", __name__)
